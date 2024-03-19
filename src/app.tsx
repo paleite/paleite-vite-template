1bd77e51c54e8b7219565efae623101781675589
Patrick Eriksson <paleite@users.noreply.github.com>
2024-03-19 14:43:16 +0100

    Improve rendering

--- src/app.tsx
+++ src/app.tsx
@@ -21,23 +21,31 @@ const App = () => {
   };
 
   return (
-    <main>
-      <h1>Hello, World! Clicked {count} times</h1>
-      <FancyButton
-        onClick={() => {
-          handleClick();
-        }}
-        disabled={isLoading}
-        loading={isLoading}
-      >
-        Fancy Button
-      </FancyButton>
+    <main className="mx-auto my-3 max-w-screen-sm rounded-xl bg-slate-300 p-4 py-3">
+      <section className="mb-8">
+        <h1 className="mb-2 font-semibold">
+          Hello, World! Clicked {count} times
+        </h1>
+        <FancyButton
+          onClick={() => {
+            handleClick();
+          }}
+          disabled={isLoading}
+          loading={isLoading}
+        >
+          Fancy Button
+        </FancyButton>
+      </section>
       <section>
-        <h2>Timestamps</h2>
+        <h2 className="mb-2 font-semibold">Timestamps</h2>
         <ul ref={parent}>
-        {timestamps.length === 0 ? <li className="font-black">No timestamps yet!</li> : timestamps.map((timestamp, index) => (
-            <li key={index}>{timestamp.toString()}</li>
-          ))}
+          {timestamps.length === 0 ? (
+            <li className="font-black">No timestamps yet!</li>
+          ) : (
+            timestamps.map((timestamp, index) => (
+              <li key={index}>{timestamp.toString()}</li>
+            ))
+          )}
         </ul>
       </section>
     </main>
