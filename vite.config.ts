ae4f1202060a5627b30dc24c9b13fa39d355a0cc
Patrick Aleite <paleite@users.noreply.github.com>
2024-12-01 10:16:02 +0100

    Remove behavior with baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { name } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${name}/`,
   plugins: [react()],
 });
